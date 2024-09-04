7bd62b7c193218e91e8039e32b9fbe408f5f6535
Mathias Lühr <mathias.luehr@gmx.de>
2024-09-04 23:08:16 +0200

    feat: adds implementation for var substitution

--- tests/test_envjson.py
+++ tests/test_envjson.py
@@ -8,16 +8,22 @@ import pytest
 from envjson import EnvJSON
 
 # set os env
-os.environ["TEST_ENV"] = "test-env"
+os.environ["DB_HOST"] = "substituted_host"
+os.environ["DB_PORT"] = "1433"
+os.environ["DB_USERNAME"] = "substituted_user"
+os.environ["DB_PASSWORD"] = "substituted_password"
+os.environ["DB_SERVER"] = "substituted_server"
 
 
-def test_it_should_return_default_value():
-    env = EnvJSON(
-        json_file="tests/test.json", strict=True
+def test_it_should_return_substituted_value():
+    env = EnvJSON(json_file="tests/test.json", strict=True)
+    assert env["db"]["host"] == "substituted_host"
+    assert env["db"]["port"] == "1433"
+    assert env["db"]["username"] == "substituted_user"
+    assert env["db"]["password"] == "substituted_password"
+    assert env["db"]["name"] == "my_database"
+    assert (
+        env["db"]["connectionString"]
+        == "substituted_user:substituted_password@substituted_server"
     )
-    print(env)
-    # assert env["simple_d"] is None
-    # assert env["simple_e"] == ""
-    # assert env["config.complex"] == "xxxXyyy"
-    # assert env["config.with_default"] == "DEFAULT"
 
